ad98a26e3efc09c54f495b6c16b8e719998a4832
Jackpot0815 <90830935+Jackpot0815@users.noreply.github.com>
2023-01-11 00:26:12 +0100

    Letzte Version

--- RPSErweitert.py
+++ RPSErweitert.py
@@ -1,82 +1,132 @@
 import random
+import json
 
-auswahl = ["s", "r", "p", "sp", "e"]
+auswahl = ["stein", "papier", "schere", "spock", "echse"]
+Speicher = {"spieler": 0, "cpu": 0, "unentschieden": 0, "stein": 0, "papier": 0, "schere": 0, "spock": 0, "echse": 0}
+dataFile = {}
+gewichtung = [0, 0, 0, 0, 0]
 
 
-def computer(alist):
-    random.shuffle(alist)
-    com = alist[0]
-    return com
+def combine(a, b):
+    for i in a:
+        dataFile[i] = a[i] + b[i]
+    return a
+
+
+def reset():
+    dataFile = {"spieler": 0, "cpu": 0, "unentschieden": 0, "stein": 0, "schere": 0, "papier": 0, "spock": 0, "echse": 0}
+    Speicher = dataFile
+    gewichtung = [0, 0, 0, 0, 0]
+    f = open("Speicher.txt", "w")
+    json.dump(dataFile, f)
+    f.close()
+
+
+def update():
+    f = open("Speicher.txt", "r")
+    save = f.read()
+    if save:
+        save_json = json.loads(save)
+        combine(Speicher, save_json)
+    f.close()
+    f = open("Speicher.txt", "w")
+    json.dump(dataFile, f)
+    f.close()
+
+
+def CPU():
+    count = 0
+    for i in dataFile:
+        if "spieler" not in i and "cpu" not in i and "unentschieden" not in i:
+            if dataFile[i] == 0:
+                gewichtung[count] = dataFile[i] + 0.01
+            else:
+                gewichtung[count] = dataFile[i]
+            count += 1
+
+    move = gewichtung[len(gewichtung) - 1]
+    gewichtung.remove(gewichtung[len(gewichtung) - 1])
+    gewichtung.insert(0, move)
+    choice = random.choices(auswahl, k=1, weights=gewichtung)
+    return choice[0]
+
+
+def Gewinner(player, cpu):
+    spielerWahl = auswahl.index(player)
+    cpuWahl = auswahl.index(cpu)
+    if (spielerWahl + 2) > 4:
+        spielerWahl = spielerWahl - 5
+    if (auswahl[spielerWahl + 2] == auswahl[cpuWahl]) or (auswahl[spielerWahl - 1] == auswahl[cpuWahl]):
+        gewinner = "Spieler hat gewonnen!"
+        Speicher["spieler"] += 1
+    elif (auswahl[spielerWahl - 2] == auswahl[cpuWahl]) or (auswahl[spielerWahl + 1] == auswahl[cpuWahl]):
+        gewinner = "Computer hat gewonnen!"
+        Speicher["cpu"] += 1
+    else:
+        gewinner = "Unentschieden!"
+        Speicher["unentschieden"] += 1
+    return gewinner
 
 
 def game():
-    user = input("Deine Wahl: ")
-    if user in auswahl:
-        com = computer(auswahl)
-        print("Computer: " + com)
-
-        # Unentschieden
-        if com == user:
-            print("Unentschieden!")
-
-        # Siccors/Schere
-        elif com == "s" and user == "p":
-            print("Du hast verloren!")
-        elif com == "s" and user == "r":
-            print("Du hast gewonnen!")
-        elif com == "s" and user == "sp":
-            print("Du hast gewonnen!")
-        if com == "s" and user == "e":
-            print("Du hast verloren!")
-
-        # Rock/Stein
-        elif com == "r" and user == "p":
-            print("Du hast gewonnen!")
-        elif com == "r" and user == "s":
-            print("Du hast verloren!")
-        if com == "r" and user == "sp":
-            print("Du hast gewonnen!")
-        if com == "r" and user == "e":
-            print("Du hast verloren!")
-
-        # Paper/Papier
-        elif com == "p" and user == "s":
-            print("Du hast gewonnen!")
-        if com == "p" and user == "r":
-            print("Du hast verloren!")
-        if com == "p" and user == "sp":
-            print("Du hast verloren!")
-        if com == "p" and user == "e":
-            print("Du hast gewonnen!")
-
-        # Spock
-        if com == "sp" and user == "s":
-            print("Du hast verloren!")
-        if com == "sp" and user == "r":
-            print("Du hast verloren!")
-        if com == "sp" and user == "p":
-            print("Du hast gewonnen!")
-        if com == "sp" and user == "e":
-            print("Du hast gewonnen!")
-
-        # Echse
-        if com == "e" and user == "s":
-            print("Du hast gewonnen!")
-        if com == "e" and user == "r":
-            print("Du hast gewonnen!")
-        if com == "e" and user == "p":
-            print("Du hast verloren!")
-        if com == "e" and user == "sp":
-            print("Du hast verloren!")
-    else:
-        print("Eingabe ist falsch")
-
-
-def main():
-    print("Spiel: Schere-Stein-Papier-Spock-Echse")
-    print("Bitte wähle: Schere (s), Stein (r), Papier(p), Spock(sp), Echse (e)")
-    game()
+    update()
+    counter = 1
+    print("Spiel: Schere-Stein-Papier-Spock-Echse\n")
+    while counter == 1:
+        print("Menü, wählen Sie:")
+        print(">SPIEL STARTEN (spielen)")
+        print(">STATS (stats)")
+        print(">SPEICHERN (speichern)")
+        print(">STATS NEU SETZEN (neusetzen)")
+        print(">VERLASSEN (verlassen)")
+        choice = str(input().lower())
+        if choice == "spielen":
+            status = 1
+            while status == 1:
+                print("Bitte wähle: Schere, Stein, Papier, Spock, Echse")
+                spielerWahl = str(input().lower())
+                if auswahl.count(spielerWahl) == 0:
+                    print("Wählen Sie bitte eine existierende Form aus!")
+                else:
+                    Speicher[spielerWahl] += 1
+                    cpuWahl = CPU()
+                    print("\nDeine Wahl: ", spielerWahl.upper())
+                    print("Wahl der CPU: ", cpuWahl.upper())
+                    resultat = Gewinner(spielerWahl, cpuWahl)
+                    print("\nErgebnis: ", resultat)
+                    cont = 1
+                    while cont == 1:
+                        print("\nNoch einmal spielen? (Ja/Nein)")
+                        decision = input().lower()
+                        if decision == "ja":
+                            status = 1
+                            cont = 0
+                        elif decision == "nein":
+                            cont = 0
+                            status = 0
+                        else:
+                            cont = 1
+                            status = 0
+            counter = 1
+        elif choice == "stats":
+            print(dataFile)
+            print()
+            counter = 1
+        elif choice == "speichern":
+            update()
+            print("Deine Stats wurden geupdated!\n")
+            counter = 1
+        elif choice == "neusetzen":
+            reset()
+            print("Deine gespeicherten Daten wurden zurückgesetzt!\n")
+            counter = 1
+        elif choice == "verlassen":
+            counter = 0
+        else:
+            counter = 1
+            print("Wählen Sie bitte eine valide Auswahl!\n")
 
 
 if __name__ == '__main__':
-    main()
+    update()
+    game()
